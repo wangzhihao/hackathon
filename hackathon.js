09597e4c618652f6226dd31736c21b75022690c0
zhihao <zhihaow@amazon.com>
2017-03-29 19:06:08 +0800

    add dismiss

--- hackathon.js
+++ hackathon.js
@@ -1,7 +1,12 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-	$(".sc-mobile-icon.sc-menu-trigger.sc-snes-top-a").on('click', function(){
+	$(".sc-mobile-icon.sc-menu-trigger.sc-snes-top-a").on('click', function(e){
 		$("#spaui").toggleClass("hack_animate");
+		e.preventDefault();
+	});
+	$("#spaui-close").on('click', function(e){
+		$("#spaui").toggleClass("hack_animate");
+		e.preventDefault();
 	});
 
 	$("#amazon-sort").on('click', function() {
