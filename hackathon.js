2c4e1f4b06054727de3886c4a18cff30d68a5267
zhihao <zhihaow@amazon.com>
2017-03-29 16:21:28 +0800

    sort

--- hackathon.js
+++ hackathon.js
@@ -2,5 +2,9 @@
 $( document ).ready(function() {
 	$(".sc-mobile-icon.sc-menu-trigger.sc-snes-top-a").on('click', function(){
 		$("#spaui").toggleClass("hack_animate");
-	})
+	});
+
+	$("#amazon-sort").on('click', function() {
+		$("#amazon-list").toggleClass("sorted");
+	});
 });
