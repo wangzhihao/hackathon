bac847ea03d3d3aee8dae2713d30de20ec8a2100
zhihao <zhihaow@amazon.com>
2017-03-29 13:30:03 +0800

    add css and js

---
+++ hackathon.js
@@ -0,0 +1,6 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+	$(".sc-mobile-icon.sc-menu-trigger.sc-snes-top-a").on('click', function(){
+		$("#spaui").toggleClass("hack_animate");
+	})
+});
