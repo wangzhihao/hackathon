50b46b6fcd5728d96b7b740716ff5a7287f8439a
zhihao <zhihaow@amazon.com>
2017-03-29 12:55:44 +0800

    add popup

--- resources/rainier-payments-redrive-report-2684012930._V1_.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function(){jQuery(".regenerateButton").click(function(){jQuery(this).replaceWith(jQuery(".progressText").html());
-jQuery.post("/gp/payments-account/redrive.html",{groupId:jQuery(this).attr("id")});
-});
-});
