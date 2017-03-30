58ef4e62a8b36fccd5b36b8fa5a896d3492330a5
zhihao <zhihaow@amazon.com>
2017-03-30 12:09:28 +0800

    add remove effect

--- hackathon.js
+++ hackathon.js
@@ -1,5 +1,17 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
+
+	function removeElement(event) {
+
+		if (event.animationName === 'disapear') {
+
+			event.target.parentNode.removeChild(event.target);
+		}
+	}
+
+	document.body.addEventListener('animationend', removeElement);
+	document.body.addEventListener('webkitAnimationEnd', removeElement);
+
 	$(".sc-mobile-icon.sc-menu-trigger.sc-snes-top-a").on('click', function(e){
 		$("#spaui").toggleClass("hack_animate");
 		e.preventDefault();
@@ -15,7 +27,11 @@ $( document ).ready(function() {
 		$("#spaui", window.parent.document).toggleClass("hack_animate");
 		e.preventDefault();
 	});
-	
+
+	$("#amazon-list .remove").on('click', function(e){
+		$(this).closest(".list-group-item").addClass('removed');
+		e.preventDefault();
+	});
 
 	$("#amazon-sort").on('click', function() {
 		$("#amazon-list").toggleClass("sorted");
